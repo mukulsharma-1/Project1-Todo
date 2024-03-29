b2114bb8a4ba737350decfdca2ed7ab67ff65747
Mukul Sharma <ms335273@gmail.com>
2024-03-29 23:02:08 +0530

    Updating Script File with Some Functionalities

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const todoForm = document.querySelector('form');
 const todoInput = document.getElementById('todo-input');
 const todoListUL = document.getElementById('todo-list');
+
+let allTodos = getTodos();
+updateTodoList();
+
+todoForm.addEventListener('submit', function(event){
+    event.preventDefault();
+    addTodo();
+})
