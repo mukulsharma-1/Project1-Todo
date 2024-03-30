2212bf65ca991d8ca3f742c0e290ca6b8bc1b9b2
Mukul Sharma <ms335273@gmail.com>
2024-03-30 23:31:00 +0530

    Adding Function to Script

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ todoForm.addEventListener('submit', function(event){
     event.preventDefault();
     addTodo();
 })
+
+function addTodo(){
+    const todoText = todoInput.value.trim();
+    if(todoText.length > 0){
+        const todoObject = {
+            text: todoText,
+            completed: false
+        }
+        allTodos.push(todoObject);
+        updateTodoList();
+        saveTodos();
+        todoInput.value = "";
+    }  
+}
